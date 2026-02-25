ed6d107ad3b1216bbe94f0e59d4b66c358459288
Vaishakh Vinod <vaishakhvinodnlr@gmail.com>
2026-02-25 23:37:10 +0530

    Initial commit

---
+++ server/src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+         console.log(`MongoDB is Connected: ${conn.connection.host}`);
+    } catch(error){
+        console.error("Database connection failed:", error.message);
+    process.exit(1);
+    }
+}
+module.exports = connectDB;
